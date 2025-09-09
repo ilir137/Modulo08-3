966d6d0b6bc7dcccecd392abcff5ceaa40632495
ilir137 <appsilir@gmail.com>
2025-09-10 00:23:50 +0200

    Update elementos.js

--- src/elementos.js
+++ src/elementos.js
@@ -2,6 +2,7 @@ export const cartas = document.querySelectorAll('.carta');
 export const contenedor = document.getElementById('container');
 export const btnCerrarDialogo = document.getElementById("btnCerrarDialogo");
 export const dialogPartidaGanada = document.getElementById("dialogPartidaGanada");
+export const display = document.getElementById("display");
 
 const estiloRoot = getComputedStyle(document.documentElement);
 export const colorCartaVolteada = estiloRoot.getPropertyValue('--colorCartaVolteada').trim();
